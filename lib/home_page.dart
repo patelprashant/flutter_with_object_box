3049c0c21dadcc7ff413266e72c2b24e65b8c579
Prashant <prashant.patel77@gmail.com>
2021-07-25 11:13:42 +1000

    Added objectbox store and put data

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,10 @@
 import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_with_object_box/entities.dart';
+import 'package:flutter_with_object_box/objectbox.g.dart';
+import 'package:objectbox/objectbox.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart';
 
 import 'order_data.dart';
 
@@ -14,11 +19,31 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final fakerData = Faker();
+  late Store _store;
+  bool hasInitialised = false;
+
+  late CustomerData _customerData;
 
   @override
   void initState() {
     super.initState();
     setNewCustomer();
+    getApplicationDocumentsDirectory().then((dir) {
+      _store = Store(
+        getObjectBoxModel(),
+        directory: join(dir.path, 'objectbox'),
+      );
+
+      setState(() {
+        hasInitialised = true;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _store.close();
+    super.dispose();
   }
 
   @override
@@ -45,12 +70,14 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void setNewCustomer() {
-    //TODO: Implement
-    print('Name:${fakerData.person.name()}');
+    _customerData = CustomerData(name: fakerData.person.name());
   }
 
   void addFakeOrderForCustomer() {
-    //TODO: Implement
-    print('Price:${fakerData.randomGenerator.integer(500, min: 10)}');
+    final custOrder =
+        CustOrder(price: fakerData.randomGenerator.integer(500, min: 10));
+
+    custOrder.customer.target = _customerData;
+    _store.box<CustOrder>().put(custOrder);
   }
 }
